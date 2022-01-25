333145c1189b6cb3b70ac545d14879d67400ce25
hnslyswhy <huanyuw01@gmail.com>
2022-01-25 15:44:38 -0800

    fixed new user login issue

--- server/passport.js
+++ server/passport.js
@@ -19,7 +19,9 @@ passport.use(
 );
 
 passport.serializeUser((user, cb) => {
+  console.log(user);
   user.then((userData) => {
+    console.log(userData);
     // This callback expects a user.id.
     // See stackoverflow: https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
     cb(null, userData.id);
@@ -28,6 +30,7 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser((userId, cb) => {
   let user = findOrCreateUser("google", { id: userId });
+  console.log(user);
   cb(null, user);
 });
 
@@ -55,27 +58,12 @@ const findOrCreateUser = async (issuer, profile) => {
         .collection(issuer)
         .insertOne(profile);
 
-      return creatResult;
+      return profile;
     } else {
       return result;
     }
   } catch (e) {
     console.error(e);
   } finally {
-  }
-  const result = await client
-    .db("aviator")
-    .collection(issuer)
-    .findOne({ id: profile.id });
-
-  if (!result) {
-    const creatResult = await req.dbClient
-      .db("aviator")
-      .collection(issuer)
-      .insertOne(profile);
-
-    return creatResult;
-  } else {
-    return result;
   }
 };
